cdb84a28f1bddb1072cdee062bbe7fb2e72a5e1d
Top-Thanwa <phanetramtop@gmail.com>
2025-07-24 09:43:22 +0700

    Update North.sql

--- North.sql
+++ North.sql
@@ -1,22 +1,22 @@
 select * from customers
 
---1
+--1. ต้องการรหัสพนักงาน คำนำหน้า ชื่อ นามสกุล ของพนักงานที่อยู่ในเมือง London
 select employeeid,titleofcourtesy,firstname,lastname
 from Employees
 where city='london'
 
---2
+--2. ต้องการข้อมูลสินค้าที่มีรหัสประเภท 1,2,4,8 และมีราคา ช่วง 50-100$
 select * from Products
-where ProductID = '1' or ProductID = '2' or ProductID = '4' or ProductID = '8'and unitprice between 50 and 100
+where CategoryID in (1,2,3,4) and unitprice between 50 and 100
 
---3
+--3. ต้องการประเทศ เมือง ชื่อบริษัทลูกค้า ชื่อผู้ติดต่อ เบอร์โทร ของลูกค้าทั้งหมด
 select country,city,companyname,contactname,phone
 from customers
 
---4
+--4. ข้อมูลของสินค้ารหัสประเภทที่ 1 ราคาไม่เกิน 50 หรือสินค้ารหัสประเภทที่ 8 ราคาไม่เกิน 75
 select * from Products
-where ProductID = '1' and unitprice <50 or ProductID = '8' and unitprice <75
+where (CategoryID = 1 and unitprice <=50) or (CategoryID = 8 and unitprice <=75)
 
---5
+--5. ชื่อบริษัทลูกค้า ที่อยู่ใน ประเทศ USA ที่ไม่มีหมายเลข FAX  เรียงตามลำดับชื่อบริษัท 
 select CompanyName from customers
 where country = 'USA' and   Fax is null order by CompanyName
